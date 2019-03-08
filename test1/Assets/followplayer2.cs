b2b403f587ad2a60832a20b6b87c44c83f0449c6
Joonsoo Kim <daniel.kim365@hotmail.com>
2019-03-07 23:15:04 -0500

    new code

---
+++ test1/Assets/followplayer2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class followplayer2 : MonoBehaviour
+{
+    public Transform player2;
+    public Vector3 offset2;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player2.position + offset2;
+    }
+}
---
+++ test1/Assets/score.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.IO.Ports;
+using System.Collections;
+public class score : MonoBehaviour
+{
+    public Text scoreText;
+    SerialPort sp = new SerialPort("COM3", 9600);
+    // Update is called once per frame
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+    void Update()
+    {
+        if (sp.IsOpen)
+        {
+            try
+            {
+                scoreText.text = sp.ReadLine().ToString();
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+      
+    }
+}
